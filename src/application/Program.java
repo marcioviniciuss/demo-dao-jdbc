c4be0c71a6090cbcdd05cca309182cbc8b1abfa4
Vinicius <vmarcio469@gmail.com>
2025-11-10 22:20:16 -0300

    implementacao do metodo 'findById'

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,13 +10,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1,"books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "Carlos", "carlos@gmail.com", new Date(), 3000.00,
-                obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
