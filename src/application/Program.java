e07542c3996c119d1377badefdb20b7b67e22416
Vinicius <vmarcio469@gmail.com>
2025-11-10 23:05:09 -0300

    adicao finally SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
